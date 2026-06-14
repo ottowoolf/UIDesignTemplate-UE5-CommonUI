4200aee7af670085d80ffc20217e6da807f8687e
Otman Oulad Chaib Ouali <59646347+ottowoolf@users.noreply.github.com>
2026-06-14 21:43:37 +1000

    Description side panel

---
+++ Source/FrontendTemplate_CUI/Private/Widgets/Options/Widget_OptionsDetailsView.cpp
@@ -0,0 +1,5 @@
+// Never Too Late Games
+
+
+#include "Widgets/Options/Widget_OptionsDetailsView.h"
+
---
+++ Source/FrontendTemplate_CUI/Public/Widgets/Options/Widget_OptionsDetailsView.h
@@ -0,0 +1,17 @@
+// Never Too Late Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Widget_OptionsDetailsView.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTENDTEMPLATE_CUI_API UWidget_OptionsDetailsView : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
