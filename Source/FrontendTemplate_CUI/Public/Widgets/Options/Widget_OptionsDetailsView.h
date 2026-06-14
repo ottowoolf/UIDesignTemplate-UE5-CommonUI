98cec039efcc37e01772f6d626cc7aa4afe317a7
Otman Oulad Chaib Ouali <59646347+ottowoolf@users.noreply.github.com>
2026-06-14 21:43:48 +1000

    Adding description side panel

--- Source/FrontendTemplate_CUI/Public/Widgets/Options/Widget_OptionsDetailsView.h
+++ Source/FrontendTemplate_CUI/Public/Widgets/Options/Widget_OptionsDetailsView.h
@@ -6,12 +6,36 @@
 #include "Blueprint/UserWidget.h"
 #include "Widget_OptionsDetailsView.generated.h"
 
+
+class UCommonTextBlock;
+class UCommonLazyImage;
+class UCommonRichTextBlock;
 /**
- * 
+ *
  */
-UCLASS()
+UCLASS(abstract, BlueprintType, meta = (DisableNativeTick))
 class FRONTENDTEMPLATE_CUI_API UWidget_OptionsDetailsView : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+private:
+	//***** Bound Widgets *****//
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonTextBlock* CommonTextBlock_Title;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonLazyImage* CommonLazyImage_DescriptionImage;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonRichTextBlock* CommonRichTextBlock_Description;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonRichTextBlock* CommonRichTextBlock_DynamicDetails;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonRichTextBlock* CommonRichTextBlock_DisabledReason;
+
+
+	//***** Bound Widgets *****//
 };
