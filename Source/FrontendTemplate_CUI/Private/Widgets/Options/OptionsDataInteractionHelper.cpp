1b3dc40f8e4fe132efae71f4c973e0efbcd8f740
Otman Oulad Chaib Ouali <59646347+ottowoolf@users.noreply.github.com>
2026-06-14 12:45:53 +1000

    Stashed changes

---
+++ Source/FrontendTemplate_CUI/Private/Widgets/Options/OptionsDataInteractionHelper.cpp
@@ -0,0 +1,12 @@
+// Never Too Late Games
+
+
+#include "Widgets/Options/OptionsDataInteractionHelper.h"
+
+OptionsDataInteractionHelper::OptionsDataInteractionHelper()
+{
+}
+
+OptionsDataInteractionHelper::~OptionsDataInteractionHelper()
+{
+}
---
+++ Source/FrontendTemplate_CUI/Public/Widgets/Options/OptionsDataInteractionHelper.h
@@ -0,0 +1,15 @@
+// Never Too Late Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class FRONTENDTEMPLATE_CUI_API OptionsDataInteractionHelper
+{
+public:
+	OptionsDataInteractionHelper();
+	~OptionsDataInteractionHelper();
+};
