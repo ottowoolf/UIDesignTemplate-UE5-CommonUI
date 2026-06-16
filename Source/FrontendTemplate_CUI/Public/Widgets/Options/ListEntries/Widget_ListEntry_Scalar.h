e11346156b66e26ece8d85f4d8ebc3e2206a642e
Otman Oulad Chaib Ouali <59646347+ottowoolf@users.noreply.github.com>
2026-06-16 20:07:09 +1000

    Boiler plate for Audio Tab settings added

---
+++ Source/FrontendTemplate_CUI/Public/Widgets/Options/ListEntries/Widget_ListEntry_Scalar.h
@@ -0,0 +1,17 @@
+// Never Too Late Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Options/ListEntries/Widget_ListEntry_Base.h"
+#include "Widget_ListEntry_Scalar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTENDTEMPLATE_CUI_API UWidget_ListEntry_Scalar : public UWidget_ListEntry_Base
+{
+	GENERATED_BODY()
+	
+};
