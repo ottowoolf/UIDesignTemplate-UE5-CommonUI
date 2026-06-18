2f9a49e45f9f35ef1124760417fff504a207c027
Otman Oulad Chaib Ouali <59646347+ottowoolf@users.noreply.github.com>
2026-06-18 15:12:59 +1000

    Editable condition for video settings mode added.

---
+++ Source/FrontendTemplate_CUI/Public/Widgets/Options/DataObjects/ListDataObject_StringResolution.h
@@ -0,0 +1,17 @@
+// Never Too Late Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Options/DataObjects/ListDataObject_String.h"
+#include "ListDataObject_StringResolution.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTENDTEMPLATE_CUI_API UListDataObject_StringResolution : public UListDataObject_String
+{
+	GENERATED_BODY()
+	
+};
